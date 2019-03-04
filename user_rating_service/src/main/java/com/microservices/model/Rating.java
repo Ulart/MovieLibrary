7a26c7b1e605cf47038b591b53b2706ca5b29f39
Artur <artur0695@interia.pl>
2019-03-04 18:01:53 +0100

    initial commit for user_library service

--- user_rating_service/src/main/java/com/microservices/model/Rating.java
+++ user_rating_service/src/main/java/com/microservices/model/Rating.java
@@ -4,4 +4,26 @@ public class Rating {
 
 	int movieId;
 	int rating;
+	
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	public Rating(int movieId, int rating) {
+		super();
+		this.movieId = movieId;
+		this.rating = rating;
+	}
+	public Rating() {
+		super();
+	}
 }
--- user_rating_service/src/main/java/com/microservices/model/UserRatings.java
+++ user_rating_service/src/main/java/com/microservices/model/UserRatings.java
@@ -1,7 +1,32 @@
 package com.microservices.model;
 
+import java.util.List;
+
 public class UserRatings {
 
 	int userId;
+	List<Rating> ratings;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public List<Rating> getRatings() {
+		return ratings;
+	}
+	public void setRatings(List<Rating> ratings) {
+		this.ratings = ratings;
+	}
+	
+	public UserRatings(int userId, List<Rating> ratings) {
+		super();
+		this.userId = userId;
+		this.ratings = ratings;
+	}
+	public UserRatings() {
+		super();
+	}
 	
 }
